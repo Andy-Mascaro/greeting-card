2337ee21835db5ff4f12b714200a695a5ba3ab6b
Andy Mascaro <amascaro1@hotmail.com>
2021-12-07 23:36:22 -0800

    added thanksgiving

--- app.js
+++ app.js
@@ -3,4 +3,21 @@ const messageInput =
 
 //Background
 const backgroundSelect = document.getElementById('background-select');   
-const backgroundImage = document.getElementById('background-image');     
+const backgroundImage = document.getElementById('background-image');
+
+//description
+
+const descriptionTextarea = document.getElementById('message-textarea');
+const descriptionDisplay = document.getElementById('message-display');
+descriptionTextarea.addEventListener('input', () => {
+descriptionDisplay.textContent = descriptionTextarea.value;
+
+  //Export
+const exportButton = document.getElementById('export-button');
+exportButton.addEventListener('click', async () => {
+const dataUrl = await domtoimage.toPng(character);
+const link = document.createElement('a');
+link.download = nameInput.value + '.png';
+link.href = dataUrl;
+link.click();
+});
